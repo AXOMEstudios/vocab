a88866fd7f5df31e971bc6ae52eabc25acc83157
AXOME <90136144+AXOMEstudios@users.noreply.github.com>
2021-11-04 16:46:32 +0000

    fixes & bootstraps

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, redirect, session, g, request, flash
 from os import getenv
 import axomeapis.auth as auth
 import axomeapis.antixsrf as antixsrf
-import axomeapis.sitemap as sitemap
+import pymongo
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = getenv("SECRET_KEY")
+
+client = pymongo.MongoClient("mongodb+srv://vocab-server:"+getenv("DB_SECRET")+"@axodb01.kpdbk.mongodb.net/Vocab?retryWrites=true&w=majority")
 
 @app.before_request
 def before_request():
@@ -27,6 +29,6 @@ def login():
 def login_run():
   g.user = auth.login(request.args["usr"])
   flash("You have been signed in successfully", "success")
-  flash("Welcome to Vocab!", "danger")
+  return redirect("/")
 
 app.run(host="0.0.0.0", port=8080)
